2155bdfd0f04a77f2066da4e1fb5a72e2da27e7a
Geethika1504 <geethikamunikoti369@gmail.com>
2026-07-13 10:38:36 +0530

    Add files via upload

---
+++ slicing,indexing.py
@@ -0,0 +1,124 @@
+Python 3.14.4 (tags/v3.14.4:23116f9, Apr  7 2026, 14:10:54) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+a="Vijayawada"
+a[3]
+'a'
+a[5]
+'a'
+a="i am in class"
+a[8]+a[9]+a[10]+a[11]+a[12]
+'class'
+a[2]+a[3]
+'am'
+a[5]+a[6]
+'in'
+a[1]
+' '
+a[1]+a[4]+a[7]
+'   '
+a="geethika"
+a="geethika"
+a="i am learning python"
+a[6]+a[7]+a[8]+a[9]+a[10]
+'earni'
+a=a[5]+a[6]+a[7]+a[8]+a[9]
+
+#new model
+a="i am learning python"
+a[6]+a[7]+a[8]+a[9]+a[10]
+'earni'
+a[6]+a[7]+a[8]+a[9]+a[10]
+'earni'
+a[6]+a[7]+a[8]+a[9]+a[10]
+'earni'
+a[5]+a[6]+a[7]+a[8]+a[9]
+'learn'
+b="i love me"
+b[2]+b[3]+b[4]+b[5]
+'love'
+a="vijawada is a royal city"
+a[-4]+a[-3]+a[-2]+a[-1]
+'city'
+a[-13]+a[-14]
+' s'
+a[15]+a[14]
+'or'
+a[-15]+a[-14]
+'is'
+#slicing
+a="codegnan
+SyntaxError: unterminated string literal (detected at line 1)
+a="codegnan"
+a[0]+a[1]+a[3]+a[4]
+'coeg'
+a[0]+a[1]+a[2]+a[3]
+'code'
+a[0:3]
+'cod'
+a[0:4]
+'code'
+a[4:8]
+'gnan'
+a[:4]
+'code'
+a[4:]
+'gnan'
+ a="Work until you succeed"
+ 
+SyntaxError: unexpected indent
+a="work until you succeed"
+a[0:4]
+'work'
+a[:4]
+'work'
+a[5:10]
+'until'
+a[11:14]
+'you'
+a[15:22]
+'succeed'
+a[15:]
+'succeed'
+b="time is very precious"
+>>> b[13:21]
+'precious'
+>>> b[8:12]
+'very'
+>>> b[0:4]
+'time'
+>>> #negtive slicing
+>>> a="simple is better nothing"
+>>> a[-6:]
+'othing'
+>>> a[-7:]
+'nothing'
+>>> a[-18:-12]
+' is be'
+>>> a[-18:-14]
+' is '
+>>> b="simple is better than nothing"
+>>> b[-18:-14]
+'ette'
+>>> b[-16:-12]
+'ter '
+>>> b[-19:-13]
+'better'
+>>> b[-29:-25]
+'simp'
+>>> b[:-24]
+'simpl'
+>>> b[:-22]
+'simple '
+>>> #striding
+>>> a="data science"
+>>> a[::]
+'data science'
+>>> 'data science'
+'data science'
+>>> a[::1]
+'data science'
+>>> a[::2]
+'dt cec'
+>>> b="machine learning"
+>>> b[::4]
+'miln'
---
+++ strings.py
@@ -0,0 +1,247 @@
+Python 3.14.4 (tags/v3.14.4:23116f9, Apr  7 2026, 14:10:54) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+#len
+a="codegnan"
+len(a)
+8
+b="python course"
+len(b)
+13
+c=""
+len(c)
+0
+d=" "
+len(d)
+1
+e=" i am in vijayawada"
+len(e)
+19
+# number of repaeted words nd character in given string is called COUNT
+#COUNT
+a="twinkle twinkle little star"
+count(a)
+Traceback (most recent call last):
+  File "<pyshell#15>", line 1, in <module>
+    count(a)
+NameError: name 'count' is not defined. Did you mean: 'round'?
+#so we use
+a.count("twinkle")
+2
+a.count("k")
+2
+a.count("t")
+5
+a.count(" ")
+3
+a.count("")
+28
+#find a string
+a="python"
+a[1]
+'y'
+b="hello"
+b.find("l")
+2
+b[2:4]
+'ll'
+a.find("k")
+-1
+# it is opp to slicing in slicing you get number here you get character
+#ESCAPE SEQUENCES
+#\n-> new line
+#\t-> tab space
+a="name\nmailid\tmobileno\ncollege\tbranch"
+a
+'name\nmailid\tmobileno\ncollege\tbranch'
+print(a)
+name
+mailid	mobileno
+college	branch
+b="name=Geethika\nmailid:geethikamunikoti369@gmail.com\tmobileno:8233773328\ncollege:vrsec\tbranch:CSE"
+print(b)
+name=Geethika
+mailid:geethikamunikoti369@gmail.com	mobileno:8233773328
+college:vrsec	branch:CSE
+#replace()
+a="wait until you succeed"
+a.replace("wait","work")
+'work until you succeed'
+b="python java"
+b.replace("p","c")
+'cython java'
+c="wait until you succeed"
+c.replace("wait","work",1)
+'work until you succeed'
+c.replace("wait","work",1)
+'work until you succeed'
+#upper()
+a="code"
+a.upper()
+'CODE'
+#lower()
+b="HELLO"
+b.lower()
+'hello'
+c="python"
+c.upper("p")
+Traceback (most recent call last):
+  File "<pyshell#53>", line 1, in <module>
+    c.upper("p")
+TypeError: str.upper() takes no arguments (1 given)
+c[0] .upper()
+'P'
+c . capitalize()
+'Python'
+d="python course"
+d.title()
+'Python Course'
+e="i am in class"
+e.title()
+'I Am In Class'
+'I Am In Class'
+'I Am In Class'
+e.capitalize()
+'I am in class'
+a="code"
+a.isupper()
+False
+a.islower()
+True
+a.isdigiti
+Traceback (most recent call last):
+  File "<pyshell#65>", line 1, in <module>
+    a.isdigiti
+AttributeError: 'str' object has no attribute 'isdigiti'. Did you mean: 'isdigit'?
+a.isdigit()
+False
+a.isalpha()
+True
+b="code course"
+b.isalpha()
+False
+c="codecourse"
+c.isalpha
+<built-in method isalpha of str object at 0x000001CFAE678AF0>
+c.isalpha()
+True
+d="1245"
+d.isdigit()
+True
+d="1,2,3,4"
+d.isdigit()
+False
+#allnum
+a="geethika@123"
+a.isalnum()
+False
+a="geethika123"
+a.isalnum()
+True
+b="geethika.123"
+b.isalnum()
+False
+a="data science"
+a.startswith("d")
+True
+a.endswith("e")
+True
+#strip()
+#leftstrip(),rstrip()
+a="      pooja   "
+a.strip()
+'pooja'
+a.lstrip()
+'pooja   '
+a.rstrip()
+'      pooja'
+#no middle spaces applies
+#split()
+a="python java c++"
+a.split()
+['python', 'java', 'c++']
+b="i love python"
+b.split()
+['i', 'love', 'python']
+b.spliti()
+Traceback (most recent call last):
+  File "<pyshell#99>", line 1, in <module>
+    b.spliti()
+AttributeError: 'str' object has no attribute 'spliti'. Did you mean: 'split'?
+b.split(i)
+Traceback (most recent call last):
+  File "<pyshell#100>", line 1, in <module>
+    b.split(i)
+NameError: name 'i' is not defined. Did you mean: 'id'?
+b="html","css","js"
+#join()
+"'.join(b)
+SyntaxError: unterminated string literal (detected at line 1)
+"".join(b)
+'htmlcssjs'
+" ".join(b)
+'html css js'
+"k" .join(b)
+'htmlkcsskjs'
+c="python"
+"k".join(c)
+'pkyktkhkokn'
+#cancatination or joining two strings
+a="code"
+b="gnan"
+print(a+b)
+codegnan
+print(a+" "+b)
+code gnan
+c="geethika
+SyntaxError: unterminated string literal (detected at line 1)
+a="geethika"
+b="munikoti"
+print(a.title()+" "+b.title())
+Geethika Munikoti
+print((a+" "+b).title())
+Geethika Munikoti
+#formating
+a=5
+b=6
+print(a+b)
+11
+>>> print("the sum is",a+b)
+the sum is 11
+>>> city="vijaya"
+>>> print("city is",city)
+city is vijaya
+>>> print("the sum is ,a+b")
+the sum is ,a+b
+>>> #format()
+>>> a="motu"
+>>> b="pathulu"
+>>> print("hy {}{}" .format(a,b))
+hy motupathulu
+>>> print("hello {} {}" .format(a,b))
+hello motu pathulu
+>>> print("hello {}\n{}" .format(a,b))
+hello motu
+pathulu
+>>> pathulu
+Traceback (most recent call last):
+  File "<pyshell#133>", line 1, in <module>
+    pathulu
+NameError: name 'pathulu' is not defined
+>>> print("hello {}\nhello{}" .format(a,b))
+hello motu
+hellopathulu
+>>> a="sita
+SyntaxError: unterminated string literal (detected at line 1)
+>>> a='sita"
+SyntaxError: unterminated string literal (detected at line 1)
+>>> a="sita"
+>>> b="ram"
+>>> print(f"hello {a}{b}")
+hello sitaram
+>>> print(f"hello {a} {b}")
+hello sita ram
+>>> print(f"hello {a} hello {b}")
+hello sita hello ram
+>>> print(f"hello {a}\nhello {b}")
+hello sita
+hello ram
